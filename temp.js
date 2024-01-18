df84c77f5ec226f47a8ec8f2f1c46a7e467245d4
your-name <your>
2024-01-18 20:38:58 +0800

    已完成

---
+++ temp.js
@@ -0,0 +1,27 @@
+
+//計算BMI
+let hight = prompt("請輸入身高:");
+let wight = prompt("請輸入體重:");
+let bmi = wight / (hight / 100) ** 2;
+
+// 計算圓面積
+// let radius = prompt("請輸入半徑:");
+// let area = radius * radius * PI;
+// 字串+數字(自串串接)
+// let result = "面積為 " + area;
+
+/**輸出到log
+輸出網頁 (看到)
+alert 警告視窗
+ */
+//console.log(typeof (radius), area, typeof (result), bmi);
+// let result = "<ul>"
+// result += "<li>身高:" + hight + "</li>";
+// result += "<li>體重:" + wight + "</li>";
+// result += "<li>BMI:" + bmi.toFixed(2) + "</li>";
+// console.log("BMI:" + bmi)
+// result += "</ul>";
+// document.write(result);
+
+
+//alert(result);
